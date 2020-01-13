52bde3461fde1e474479965b7176329a73c174b6
shka <45268944+shka86@users.noreply.github.com>
2020-01-13 22:18:59 +0900

    Update install_sphinx.py

--- install_sphinx.py
+++ install_sphinx.py
@@ -6,5 +6,5 @@ import subprocess as sp
 # ##################################
 # install tools
 cmd = "pip3 install sphinx commonmark recommonmark sphinx-markdown-tables sphinx_rtd_theme"
-ret = sp.call(cmd)
+ret = sp.call(cmd.split())
 
